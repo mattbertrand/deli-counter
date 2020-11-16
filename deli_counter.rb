70ba652b15dc9cdb766b334d1e52d1834c988313
github username <github email address>
2020-11-15 19:34:25 -0800

    commit tree 48fd80684453859909838cad815942b8fc70fd51

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,47 @@
 # Write your code here.
+
+def line(katz_deli)
+    if katz_deli.empty?
+        puts "The line is currently empty."
+    # otherwise the katz deli is not empty
+    else
+        list_of_names = katz_deli.collect.with_index(1) do |element, index|
+            "#{index}. #{element}"
+        end.join(" ")
+        puts "The line is currently: #{list_of_names}"
+    #     we would print The line is currently: 1. Logan 2. Avi 3. Spencer
+    end
+end
+
+def take_a_number(katz_deli, name)
+    katz_deli.push(name)
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+    if katz_deli.empty?
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{katz_deli.shift}."
+    end
+end
+
+# katz_deli = []
+
+# take_a_number(katz_deli, "Ada") #=> Welcome, Ada. You are number 1 in line.
+# take_a_number(katz_deli, "Grace") #=> Welcome, Grace. You are number 2 in line.
+# take_a_number(katz_deli, "Kent") #=> Welcome, Kent. You are number 3 in line.
+
+# line(katz_deli) #=> "The line is currently: 1. Ada 2. Grace 3. Kent"
+
+# now_serving(katz_deli) #=> "Currently serving Ada."
+
+# line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent"
+
+# take_a_number(katz_deli, "Matz") #=> Welcome, Matz. You are number 3 in line.
+
+# line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent 3. Matz"
+
+# now_serving(katz_deli) #=> "Currently serving Grace."
+
+# line(katz_deli) #=> "The line is currently: 1. Kent 2. Matz"
